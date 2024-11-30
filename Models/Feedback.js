be798f56269d5edb2c543c256dfed0c2205fcf2a
Menna Magdy <menna11hassanien@gmail.com>
2024-11-30 21:46:15 +0200

    model

---
+++ Models/Feedback.js
@@ -0,0 +1,27 @@
+const sequelize = require("../.config/database");
+const DataTypes = require('sequelize');
+
+const Feedback = sequelize.define("Feedback", {
+    id : {
+        type : DataTypes.BIGINT,
+        primaryKey : true,
+        autoIncrement : true
+    },
+    reservation_id : {
+        type : DataTypes.BIGINT,
+        allowNull : false
+    },
+    rating  : {
+        type : DataTypes.INTEGER,
+        allowNull : false
+    },
+    notes : {
+        type : DataTypes.STRING,
+        allowNull : true
+    }
+}, {
+    tableName: "feedbacks",
+    timestamps: false
+});
+
+module.exports = Feedback;
