bb40c50660683626fe465231a1b6aca195216344
Menna Magdy <menna11hassanien@gmail.com>
2024-12-03 20:33:36 +0200

    controller

--- controllers/SearchController.js
+++ controllers/SearchController.js
@@ -1,4 +1,4 @@
-const { where } = require("sequelize");
+const { Op , Sequelize } = require("sequelize");
 const Restaurant = require("../Models/Restaurant");
 
 //search for the restaurant by selecting a category
@@ -9,11 +9,48 @@ const searchByCategory = async (req, res) => {
     const restaurants = await Restaurant.findAll({
       where: { food_category },
     });
-    return res.status(201).json({restaurants});
+    return res.status(200).json({restaurants});
   }
-  
+
   catch (err) {
-    console.error("The checking error : \n", err);
-    return res.status(500).json({ error: "Error finding restaurants" });
+    console.error("The searching error : \n", err);
+    return res.status(500).json({ error: "Error searching restaurants" });
   }
 };
+
+//to search by the name and show the most relevant results
+const searchByName = async (req, res) => {
+    const { title_search } = req.body;
+    //check for the most relevant results to the title given
+    try {
+        const restaurants = await Restaurant.findAll({
+            //get all restaurants which have the title_search as substring in the title
+            where: {
+                title: {
+                    [Op.like]: `%${title_search}%`
+                }
+            },
+            //fetch only needed data for search results
+            attributes: [
+                'id',
+                'title',
+                'food_category',
+                'location',
+                [Sequelize.fn('POSITION', Sequelize.literal(`'${title_search}' IN title`)), 'relevance']
+            ],
+            //sort the results
+            order: [[Sequelize.literal('relevance'), 'ASC']]
+        });
+        if (restaurants.length === 0) {
+            return res.status(404).json({ message: "No restaurants found" });
+        }
+        return res.status(200).json({ restaurants });
+
+    } catch (err) {
+        console.error("The searching error: \n", err);
+        return res.status(500).json({ error: "Error searching restaurants" });
+    }
+};
+
+module.exports = { searchByName, searchByCategory };
+
