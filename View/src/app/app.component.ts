bd7065402dd6da9e012d5a7d27afbffe03d4e63d
Hany_Maged <Hany.Maged9102@gmail.com>
2024-12-13 19:22:00 +0200

    add homepage

--- View/src/app/app.component.ts
+++ View/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SignupComponent } from "./components/signup/signup.component";
+import { HomePageComponent } from "./components/homepage/homepage.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SignupComponent],
+  imports: [RouterOutlet, SignupComponent, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
