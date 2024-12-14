b517a5301f81a4edee459aee287205cfb1941108
Hany_Maged <Hany.Maged9102@gmail.com>
2024-12-14 02:21:42 +0200

    wtf where the login

--- View/src/app/app.routes.ts
+++ View/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './components/homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // {path:'/home',component:HomePageComponent}
+];
--- View/src/app/components/login/login.component.ts
+++ View/src/app/components/login/login.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  err: string = '';
+  email: string = '';
+  password: string = '';
 
+  constructor(private router: Router, private userService: UserService) {}
+
+  // async ngOnInit(): Promise<void> {
+  //   const user = await this.userService.getUser();
+  //   if (user) {
+  //     this.router.navigate(['/userProfile']);
+  //   }
+  // }
+
+  async onSubmit(): Promise<void> {
+    const data = { email: this.email, password: this.password };
+    await this.userService.login(data);
+    
+  }
+
+  routeSignup(): void {
+    this.router.navigate(['/register']);
+  }
 }
