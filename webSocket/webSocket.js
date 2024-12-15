5bf9c57ba08036c7ca213407b0128a20af3fbc05
Hany_Maged <Hany.Maged9102@gmail.com>
2024-12-15 10:15:14 +0200

    add websocket reservation

--- webSocket/webSocket.js
+++ webSocket/webSocket.js
@@ -12,13 +12,13 @@ const setupWebSocket = (server) =>{
       console.log('Received:', message);
       socket.emit('message', `Server received: ${message}`);
     });
-    
     socket.on('book Table',require('./bookTable'))
     socket.on('disconnect', () => {
       console.log('WebSocket connection closed.');
     });
-  });
 
+  }
+);
   console.log('Socket.io server is set up.');
 }
 
